f6e4778f8d8a89ac2e0e093c5d0a809fb59a1ef8
Wendy Cannon <helene.cannon@students.mq.edu.au>
2022-03-10 12:10:46 +1100

    updates to files on windows 10

--- client.java
+++ client.java
@@ -1,3 +1,25 @@
+import java.io.*;  
+import java.net.*;  
 public class client {
   
+  public InputStream getInputStream() {
+    return null;
+  }
+
+  public OutputStream getOutputStream() {
+    return null;
+  }
+
+  public synchronized void close() {}
+
+  public static void main(String[] args) {  
+    try{      
+      Socket s = new Socket("localhost",6666);  
+      DataOutputStream dout = new DataOutputStream(s.getOutputStream());  
+      dout.writeUTF("Hello Server");  
+      dout.flush();  
+      dout.close();  
+      s.close();  
+    } catch(Exception e){System.out.println(e);}  
+  }  
 }
